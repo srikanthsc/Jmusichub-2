e4879f59480bd2d5d6d14cfa2e5526a2326f9db1
bniakate <80337975+bniakate@users.noreply.github.com>
2021-04-26 11:42:49 +0200

    Add files via upload

---
+++ Project/test/musichub/business/AudioBookTest.java
@@ -0,0 +1,22 @@
+package musichub.business;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.UUID;
+
+import org.junit.Test;
+
+public class AudioBookTest {
+    @Test
+    public void AudioBookTest() {
+        UUID uuid = UUID.randomUUID();
+        AudioBook audiobook = new AudioBook("title","artiste",100,uuid.toString(),"content","english","theater");
+        assertEquals("title",audiobook.getTitle());
+        assertEquals("artiste",audiobook.getArtist());
+        assertEquals(uuid,audiobook.getUUID());
+        assertEquals("rock",audiobook.getCategory());
+        assertEquals("content",audiobook.getContent());
+        assertEquals("english",audiobook.getLanguage());
+    }
+
+}
